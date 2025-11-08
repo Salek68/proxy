d04fa01fcd000c6a5994f01ba8b2c6290ac6d95f
Salek68 <salehink15@gmail.com>
2025-11-08 09:04:42 +0330

    first commit

---
+++ api/proxy.js
@@ -0,0 +1,22 @@
+export default async function handler(req, res) {
+  const targetUrl = req.query.url;
+
+  if (!targetUrl) {
+    return res.status(400).json({ error: "Missing 'url' query parameter" });
+  }
+
+  try {
+    const response = await fetch(targetUrl, {
+      method: 'GET',
+      headers: {
+        'X-Rapidapi-Key': '26d6bc2669msh34bc749da31f3a5p10fb9ajsnbbbf46a26c5d',
+        'X-Rapidapi-Host': 'instagram-social-api.p.rapidapi.com',
+      },
+    });
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
